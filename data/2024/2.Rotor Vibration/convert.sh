77b8fa8f1b402c3ca5a66baacb33c1e632833ce8
sjkim <andrew.kim@machbase.com>
2024-08-21 09:31:21 +0900

    convert & uplod script  test & added

---
+++ data/2024/2.Rotor Vibration/convert.sh
@@ -0,0 +1,4 @@
+# input files as belows
+# g1_sensor1.csv', 'g1_sensor2.csv', 'g1_sensor3.csv', 'g1_sensor4.csv', 'g2_sensor1.csv', 'g2_sensor2.csv', 'g2_sensor3
+.csv', 'g2_sensor4.csv 
+python ./convert.py >datahub-2024-2-rotor.csv
