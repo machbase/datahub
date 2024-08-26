ad6672ffe67d4f8561f7e6e2ce75e2ceb29f574f
sjkim <andrew.kim@machbase.com>
2024-08-26 21:30:35 +0900

    rename

---
+++ data/2024/5.BCI1/conv/upload.sh
@@ -0,0 +1,2 @@
+machbase-neo shell import --input ./train.csv --method append --timeformat ns bci1
+machbase-neo shell import --input ./test.csv --method append --timeformat ns bci1
--- data/2024/5.BCI_1/conv/upload.sh
+++
@@ -1,2 +0,0 @@
-machbase-neo shell import --input ./train.csv --method append --timeformat ns bci_1
-machbase-neo shell import --input ./test.csv --method append --timeformat ns bci_1
