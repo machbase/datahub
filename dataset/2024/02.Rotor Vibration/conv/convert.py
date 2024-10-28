59b696ba809c885518efc6b49cd55f56340786cc
mondayjowa <joy.cho@machbase.com>
2024-10-28 17:43:35 +0900

    update convert file

--- dataset/2024/02.Rotor Vibration/conv/convert.py
+++ dataset/2024/02.Rotor Vibration/conv/convert.py
@@ -1,42 +1,38 @@
-import csv
+# Import necessary libraries
+import pandas as pd
 import datetime
 import os
 
-def process_csv_files(file_name, base_time):
-    results = []
+# Set directory
+directory_path = './'
 
-    for file_name in file_list:
-        # Get the part of the file name without the extension
-        file_prefix = os.path.splitext(file_name)[0]
+# Create an empty list to store DataFrames
+dataframes = []
 
-        # Dynamically create column names
-        columns = ['time', f'{file_prefix}_normal', f'{file_prefix}_type1', f'{file_prefix}_type2', f'{file_prefix}_type3']
+# Loop through all files in the directory
+for filename in os.listdir(directory_path):
+    
+    if filename.endswith('.csv'):
 
-        # Read the CSV file
-        with open(file_name, 'r') as csv_file:
-            reader = csv.reader(csv_file)
-
-            # Process each row
-            for row in reader:
-                # Convert the time value to float and add it to base_time
-                time = int(base_time + (float(row[0]) * 1000000000))
-
-                # Process each column
-                for i, value in enumerate(row[1:], start=1):
-                    results.append(f"{columns[i]},{time},{value}")
-
-    return results
-
-# List of files to process
-file_list = ['g1_sensor1.csv', 'g1_sensor2.csv', 'g1_sensor3.csv', 'g1_sensor4.csv', 'g2_sensor1.csv', 'g2_sensor2.csv', 'g2_sensor3.csv', 'g2_sensor4.csv']  # Update with actual file names
+        # set base name 
+        base_name = filename[:-4]
+        # Load the CSV file
+        df = pd.read_csv(os.path.join(directory_path, filename), names = ['time', f'{base_name}_normal', f'{base_name}_type1', f'{base_name}_type2', f'{base_name}_type3'])
+        # Transform data
+        df = df.melt(id_vars=['time'], var_name='name', value_name='value')
+        # Append the DataFrame to the list
+        dataframes.append(df)  
+        
+# Concatenate all DataFrames into a single DataFrame
+combined_df = pd.concat(dataframes, ignore_index=True)
 
 # Process files and get results
 epoch_2024 = int(datetime.datetime(2024, 1, 1).timestamp())
 base_time = epoch_2024 * 1000000000  # Multiply by 1 billion (convert to nano)
-for file_name in file_list:
-    output = process_csv_files(file_name, base_time)
 
-# Print results
-print("NAME,TIME,VALUE")
-for line in output:
-    print(line)
+# Apply the transformation to the 'time' column
+combined_df['time'] = combined_df['time'].apply(lambda x: int(base_time + (float(x) * 1000000000)))
+
+# Save DataFrame
+combined_df.to_csv('./datahub-2024-2-rotor.csv', index=False)
+
