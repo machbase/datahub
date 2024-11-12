45af25ba601fb5a21ef405e08a2dc133d094d654
mondayjowa <joy.cho@machbase.com>
2024-11-12 13:41:34 +0900

    15.NASA bearing dataset 2 update

---
+++ dataset/2024/15.NASA Bearing2/conv/convert.sh
@@ -0,0 +1,7 @@
+python ./convert_nasa.py s1-c ./1st_test/1st_test >s1.csv
+
+python ./convert_nasa.py s2-c ./2nd_test/2nd_test/ >s2.csv
+
+python ./convert_nasa.py s3-c ./3rd_test/4th_test/txt/ >s3.csv
+
+
---
+++ dataset/2024/15.NASA Bearing2/conv/upload.sh
@@ -0,0 +1,5 @@
+machbase-neo shell import --input ./s1.csv --method append --timeformat ns bearing
+
+machbase-neo shell import --input ./s2.csv --method append --timeformat ns bearing
+
+machbase-neo shell import --input ./s3.csv --method append --timeformat ns bearing
